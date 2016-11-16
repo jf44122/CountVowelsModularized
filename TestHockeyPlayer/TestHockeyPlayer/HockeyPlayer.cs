bfa8ebbcae7010d64e9878c9d40d4f6dd34af857
Jonathan <Jonathan@TRADING>
2016-11-15 22:36:18 -0500

    Started Class Creation

---
+++ TestHockeyPlayer/TestHockeyPlayer/HockeyPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHockeyPlayer
+{
+    class HockeyPlayer
+    {
+        string name { get; set; }
+        int number { get; set; }
+        int goals { get; set; }
+
+
+    }
+}
