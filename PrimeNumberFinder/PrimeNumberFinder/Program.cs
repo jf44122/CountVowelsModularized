0483cb158ae5a9863f6fd4cf518d9e1a58a99ddd
Jonathan <Jonathan@TRADING>
2016-12-29 23:12:56 -0500

    Project Completed

---
+++ PrimeNumberFinder/PrimeNumberFinder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace PrimeNumberFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Please enter a number to see if that number is prime: ");
+            int input = Convert.ToInt32(ReadLine());
+            if (isPrime(input))
+                Write(input+" is prime.");
+        }
+        public static bool isPrime(int num)
+        {
+            if (num < 2)
+                return false;
+            if (num == 2)
+                return true;
+            for(int c=2; c<=(Convert.ToInt32(Math.Sqrt(num))); ++c)
+            {
+                if (num % c == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- PrimeNumberFinder/PrimeNumberFinder/Properties/AssemblyInfo.cs
+++ PrimeNumberFinder/PrimeNumberFinder/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestHockeyPlayer")]
+[assembly: AssemblyTitle("PrimeNumberFinder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestHockeyPlayer")]
+[assembly: AssemblyProduct("PrimeNumberFinder")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aca45ae2-bb55-4ad3-b16f-ac39cc2dd6ec")]
+[assembly: Guid("861ab36c-c369-4a2e-b839-cb5259e1e7d2")]
 
 // Version information for an assembly consists of the following four values:
 //
